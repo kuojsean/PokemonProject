1cdadb0b29bd1a973d9b691f39ed07ae96ffa26f
kuojsean <kuo.j.sean@gmail.com>
2017-12-12 09:04:20 -0700

    Created the runner method and got the controller method started.

--- src/pokemon/controller/PokemonController.java
+++ src/pokemon/controller/PokemonController.java
@@ -1,6 +1,14 @@
 package pokemon.controller;
 
+import pokemon.model.*;
+import pokemon.view.*;
+
 public class PokemonController
 {
 
+	
+	public void start()
+	{
+		
+	}
 }
--- src/pokemon/controller/PokemonRunner.java
+++ src/pokemon/controller/PokemonRunner.java
@@ -1,6 +1,14 @@
 package pokemon.controller;
-
+/**
+ * This is the runner of the PokemonProject
+ * @author skuo7320
+ *@version 12/12/17
+ */
 public class PokemonRunner
 {
-
+	public static void main(String [] args)
+	{
+		PokemonController app = new PokemonController();
+		app.start();
+	}
 }
