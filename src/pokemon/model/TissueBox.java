45fb79be3c87e55b0f266828e2d0d5b9f462a0f9
kuojsean <kuo.j.sean@gmail.com>
2017-12-14 09:03:05 -0700

    Created the tissuebox method to display inheritance and interface.

--- src/pokemon/model/TissueBox.java
+++ src/pokemon/model/TissueBox.java
@@ -1,6 +1,27 @@
 package pokemon.model;
 
-public class TissueBox
+public class TissueBox extends Pokemon implements Derptastic
 {
 
+	public TissueBox(int number, String name)
+	{
+		super (number, name);
+	}
+	
+	public int annoy(int level)
+	{
+		int annoyance = -913124;
+				
+		return annoyance;
+	}
+	
+	public String sayDerpy()
+	{
+		return "derp derp derp derp derp derp derp";
+	}
+	
+	public void derpDerpDerp()
+	{
+		
+	}
 }
