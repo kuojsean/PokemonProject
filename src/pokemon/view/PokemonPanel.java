e5249bc769722f767734b3a6faefe35e8cd1599a
kuojsean <kuo.j.sean@gmail.com>
2017-12-18 09:32:47 -0700

    Created the variables for all of the labels and areas regarding the information of pokemon.

--- src/pokemon/view/PokemonPanel.java
+++ src/pokemon/view/PokemonPanel.java
@@ -13,11 +13,26 @@ import java.awt.*;
 public class PokemonPanel extends JPanel
 {
 	private PokemonController appController;
+	private SpringLayout baseLayout;
+	private JLabel numberLabel;
+	private JLabel nameLabel;
+	private JLabel canEvolveLabel;
+	private JLabel healthPointsLabel;
+	private JLabel attackPointsLabel;
+	private JLabel enhancementModifierLabel;
+	private JTextArea number;
+	private JTextArea name;
+	private JCheckBox canEvolve;
+	private JTextArea healthPoints;
+	private JTextArea attackPoints;
+	private JTextArea enhancementModifier;
 	
 	public PokemonPanel(PokemonController appController)
 	{
 		super();
 		this.appController = appController;
+		
+		baseLayout = new SpringLayout();
 		
 		setupPanel();
 		setupLayout();
