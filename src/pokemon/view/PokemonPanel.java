0e1c7a94a0eb57d54ddec75b78eab476447e0585
kuojsean <kuo.j.sean@gmail.com>
2018-01-02 07:55:33 -0700

    created all components in the PokemonPanel and initialized them in the setupPanel.

--- src/pokemon/view/PokemonPanel.java
+++ src/pokemon/view/PokemonPanel.java
@@ -26,6 +26,13 @@ public class PokemonPanel extends JPanel
 	private JTextArea healthPoints;
 	private JTextArea attackPoints;
 	private JTextArea enhancementModifier;
+	private JTextArea pokemonPicture;
+	private JTextArea firstTextBox;
+	private JTextArea secondTextBox;
+	private JButton load;
+	private JButton reset;
+	private JButton save;
+	private JComboBox pokemonList;
 	
 	public PokemonPanel(PokemonController appController)
 	{
@@ -33,6 +40,25 @@ public class PokemonPanel extends JPanel
 		this.appController = appController;
 		
 		baseLayout = new SpringLayout();
+		numberLabel = new JLabel("#");
+		nameLabel = new JLabel ("Name");
+		canEvolveLabel = new JLabel ("Can Evolve");
+		healthPointsLabel = new JLabel ("Health");
+		attackPointsLabel = new JLabel ("Attack");
+		enhancementModifierLabel = new JLabel ("Enhancement");
+		number = new JTextArea();
+		name = new JTextArea();
+		canEvolve = new JCheckBox();
+		healthPoints = new JTextArea();
+		attackPoints = new JTextArea();
+		enhancementModifier = new JTextArea();
+		pokemonPicture = new JTextArea();
+		firstTextBox = new JTextArea();
+		secondTextBox = new JTextArea();
+		load = new JButton();
+		reset = new JButton();
+		save = new JButton();
+		pokemonList = new JComboBox();
 		
 		setupPanel();
 		setupLayout();
@@ -41,7 +67,27 @@ public class PokemonPanel extends JPanel
 	
 	private void setupPanel()
 	{
-		
+		this.setBackground(Color.RED);
+		this.setLayout(baseLayout);
+		this.add(numberLabel);
+		this.add(nameLabel);
+		this.add(canEvolveLabel);
+		this.add(healthPointsLabel);
+		this.add(attackPointsLabel);
+		this.add(enhancementModifierLabel);
+		this.add(number);
+		this.add(name);
+		this.add(canEvolve);
+		this.add(healthPoints);
+		this.add(attackPoints);
+		this.add(enhancementModifier);
+		this.add(pokemonPicture);
+		this.add(firstTextBox);
+		this.add(secondTextBox);
+		this.add(load);
+		this.add(reset);
+		this.add(save);
+		this.add(pokemonList);
 	}
 	
 	private void setupLayout()
